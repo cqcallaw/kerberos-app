1a8dc3211616c461584779ab06451c2578e96022
Caleb Callaway <enlightened.despot@gmail.com>
2012-09-15 13:04:58 -0700

    License header for state classes; basic validation of state graph at construction time

--- src/net/brainvitamins/state/FiniteStateGraph.java
+++ src/net/brainvitamins/state/FiniteStateGraph.java
@@ -1,7 +1,23 @@
 package net.brainvitamins.state;
 
+/*
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -25,15 +41,28 @@ public final class FiniteStateGraph {
 
 	public FiniteStateGraph(Map<Vertex, Set<Edge>> map, Vertex startState) {
 		super();
+
+		// validate input: can't have two edges to the same vertex
+		for (Map.Entry<Vertex, Set<Edge>> entry : map.entrySet()) {
+			for (Edge edge : entry.getValue()) {
+				Set<Edge> edgeSet = new HashSet<Edge>(entry.getValue());
+				edgeSet.remove(edge);
+				for (Edge otherEdge : edgeSet) {
+					if (otherEdge.getEnd() == edge.getEnd()) {
+						throw new IllegalArgumentException("Duplicate edge "
+								+ entry.getKey() + otherEdge.toString());
+					}
+				}
+			}
+		}
+
 		this.map = new HashMap<Vertex, Set<Edge>>(map);
-		//TODO: validation (e.g. each edge in the edge set must lead to a different target state)
 		this.currentState = startState;
 	}
 
 	public synchronized void transition(Vertex targetState) {
 		for (Edge edge : map.get(currentState)) {
-			if (edge.getEnd() == targetState) // reference equality
-			{
+			if (edge.getEnd() == targetState) { // reference equality
 				Log.d(LOG_TAG, "Transitioning to state "
 						+ edge.getEnd().getName());
 				edge.getAction().run();
@@ -44,7 +73,7 @@ public final class FiniteStateGraph {
 			}
 		}
 
-		Log.e(LOG_TAG, "Cannot transition from state "
-				+ currentState.getName() + " to state " + targetState.getName());
+		Log.e(LOG_TAG, "Cannot transition from state " + currentState.getName()
+				+ " to state " + targetState.getName());
 	}
 }
